37271f6fa1901def723ea992d6dc4f2e16c7827b
Aviwe Ngqukumba <aviwe3226@gmail.com>
2018-11-21 14:14:57 +0200

    create server and routes

--- index.js
+++ index.js
@@ -1,62 +1,71 @@
-const express = require('express');
-const session = require('express-session');
-const exphbs = require('express-handlebars');
-const flash = require('express-flash');
-const bodyParser = require('body-parser');
+const express = require("express");
+const session = require("express-session");
+const exphbs = require("express-handlebars");
+const flash = require("express-flash");
+const bodyParser = require("body-parser");
 const pg = require("pg");
-const FactoryFunction = require('./services/factory');
-const Routes = require('./routes/routes');
+const FactoryFunction = require("./services/factory");
+const Routes = require("./routes/routes");
 const Pool = pg.Pool;
 let useSSL = false;
 let local = process.env.LOCAL || false;
 if (process.env.DATABASE_URL && !local) {
-    useSSL = true;
+  useSSL = true;
 }
-const connectionString = process.env.DATABASE_URL || 'postgresql://coder:pg123@localhost:5432/taxee_app_database';
+const connectionString =
+  process.env.DATABASE_URL ||
+  "postgresql://coder:pg123@localhost:5432/taxee_app_database";
 const pool = new Pool({
-    connectionString,
-    ssl: useSSL
+  connectionString,
+  ssl: useSSL
 });
 const Instance = FactoryFunction(pool);
 const appRoutes = Routes(Instance);
 const app = express();
 
 // Middlewares
-app.engine('handlebars', exphbs({
-    defaultLayout: 'main'
-}));
-app.set('view engine', 'handlebars');
-app.use(session({
-    secret: 'taxee',
+app.engine(
+  "handlebars",
+  exphbs({
+    defaultLayout: "main"
+  })
+);
+app.set("view engine", "handlebars");
+app.use(
+  session({
+    secret: "taxee",
     resave: false,
     saveUninitialized: true
-}));
+  })
+);
 app.use(flash());
-app.use(express.static(__dirname + '/public'));
-app.use(bodyParser.urlencoded({
+app.use(express.static(__dirname + "/public"));
+app.use(
+  bodyParser.urlencoded({
     extended: false
-}))
-app.use(bodyParser.json())
+  })
+);
+app.use(bodyParser.json());
 
 function errorHandler(err, req, res, next) {
-    res.status(500);
-    res.render('error', {
-        error: err
-    });
+  res.status(500);
+  res.render("error", {
+    error: err
+  });
 }
 app.use(errorHandler);
 
-
-
 // Routes
-app.get('/', appRoutes.indexGet); 
-app.post('/', appRoutes.indexPost);
-// app.post('/start', appRoutes.start);
+app.get("/", appRoutes.indexGet);
+app.post("/", appRoutes.indexPost);
+app.get("/get-from-to", appRoutes.getFromTo);
+app.post("/set-from-to", appRoutes.setFromTo);
+//app.post('/start', appRoutes.start);
 // app.post('/end', appRoutes.end);
-// app.get('/trip_information',appRoutes.tripInformationGet)
-// app.post('/trip_information',appRoutes.tripInformationPost)
+app.get("/trip_information", appRoutes.tripInformationGet);
+app.post("/trip_information", appRoutes.tripInformationPost);
 
 const PORT = process.env.PORT || 3000;
-app.listen(PORT, function () {
-    console.log('Taxee-App started and listening on port:', PORT);
+app.listen(PORT, function() {
+  console.log("Taxee-App started and listening on port:", PORT);
 });
--- routes/routes.js
+++ routes/routes.js
@@ -1,38 +1,63 @@
-module.exports = function (instance) {
-    async function indexGet(req, res) {
-        // await instance.someFunction()
-        res.render('index');
-    };
-    async function indexPost(req, res) {
+module.exports = function(instance) {
+  async function indexGet(req, res) {
+    // await instance.someFunction()
+    res.render("index");
+  }
+  async function indexPost(req, res) {
+    res.render("index");
+  }
 
-        res.render('index');    
-    };
-    async function start(req, res) {
-        //start tracking user journey
-        await instance.start()
-        res.render('trip');
-    };
-    async function end(req, res) {
-        //end tracking journey and save journey
-        await instance.end()
-        res.render('trip');    
-    };
-    async function tripInformationGet(req, res) {
-        await instance.tripInfoGet()
-        res.render('trip');    
-    };
-    async function tripInformationPost(req, res) {
-        let message = await instance.tripInfoPost()
-        req.flash('info',{ message })
-        res.render('trip');    
-    };
-   
-    return {
-        indexGet,
-        indexPost,
-        start,
-        end,
-        tripInformationGet,
-        tripInformationPost,
-    };
+  async function getFromTo(req, res) {
+    let from = req.body.from;
+    let to = req.body.to;
+    let taxis = await instance.fromTo(from, to);
+    res.json(taxis);
+  }
+  async function setFromTo(req, res) {
+    let from = req.body.from;
+    let to = req.body.to;
+    let message = await instance.setFromTo(from, to);
+    req.json(message);
+    //  res.render("trip");
+  }
+
+  async function start(req, res) {
+    //start tracking user journey
+    await instance.start();
+    res.render("trip");
+  }
+  async function end(req, res) {
+    //end tracking journey and save journey
+    await instance.end();
+    res.render("trip");
+  }
+  async function tripInformationGet(req, res) {
+    let regNo = req.body.regNo;
+    let tripInfo = await instance.tripInfoGet(regNo);
+    res.json(tripInfo);
+  }
+  async function tripInformationPost(req, res) {
+    let regNo = req.body.regNo;
+    let message = await instance.tripInfoPost();
+    res.json(message);
+    //  res.render("trip");
+  }
+
+  async function getTaxisByOwner(req, res) {
+    let ownerName = req.body.fullname;
+    let taxis = await instance.getTaxisByOwner(ownerName);
+    res.json(taxis);
+  }
+
+  return {
+    indexGet,
+    indexPost,
+    getFromTo,
+    setFromTo,
+    start,
+    end,
+    tripInformationGet,
+    tripInformationPost,
+    getTaxisByOwner
+  };
 };
