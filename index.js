5ebe050abadb7d5d064b52bce7f86c5c70304ae0
michael <michaelcdavids@gmail.com>
2018-11-22 17:39:33 +0200

    request for journey from two points

--- index.js
+++ index.js
@@ -47,9 +47,65 @@ function errorHandler(err, req, res, next) {
 app.use(errorHandler);
 
 
+var request = require('request'); // replace with your client information: developer.whereismytransport.com/clients
+var CLIENT_ID = 'ea2eb61e-d200-48fa-99a7-d7940a4e76c8';
+var CLIENT_SECRET = 'e29ZFkCgcan2ec8NcM0fOVY9Ib21hZSjoerTv586Ibw=';
+var options = {
+    method: 'POST',
+    headers: 'ACCEPT: application/json',
+    url: 'https://identity.whereismytransport.com/connect/token',
+    form: {
+        client_id: CLIENT_ID,
+        client_secret: CLIENT_SECRET,
+        grant_type: 'client_credentials',
+        scope: 'transportapi:all'
+    }
+};
+request(options, function (error, response, body) {
+    var TOKEN = JSON.parse(body).access_token; // subsequent requests go here, using the TOKEN
+    var options = {
+        headers: {
+            Accept: 'application/json',
+            Authorization: 'Bearer ' + TOKEN
+        },
+        url: 'https://platform.whereismytransport.com/api/agencies'
+    };
+    request(options, function (error, response, body) {
+        console.log({
+            'Number of Agencies': JSON.parse(body).length
+        });
+    });
+    var body = {
+        geometry: {
+          type: 'Multipoint',
+          coordinates: [[18.5828324, -33.9923675], [18.416798, -33.912683]]
+        }
+      };
+      var options = {
+        method: 'POST',
+        headers: {
+          Accept: 'application/json',
+          'Content-Type': 'application/json',
+          Authorization: 'Bearer ' + TOKEN
+        },
+        url: 'https://platform.whereismytransport.com/api/journeys',
+        body: JSON.stringify(body)
+      };
+      request(options, function (error, response, body) {
+        // console.log(response);
+        
+        console.log({
+          Journeys: JSON.parse(body).itineraries[0].distance
+        });
+      });
+});
+
+
+
+
 
 // Routes
-app.get('/', appRoutes.indexGet); 
+app.get('/', appRoutes.indexGet);
 app.post('/', appRoutes.indexPost);
 // app.post('/start', appRoutes.start);
 // app.post('/end', appRoutes.end);
